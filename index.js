5bff8c8744ea28aa54a443ff6038d44a9c586928
Dirga <dirgantoro.developer@gmail.com>
2021-11-22 06:17:53 +0700

    User and Message Model

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ app.use(
 )
 
 app.get(`/user`, db.GetUsers)
+app.get('/user/:id', db.GetUserByID)
+app.post('/user', db.CreateUser)
+app.put('/user/:id', db.UpdateUser)
+app.delete('/user/:id', db.DeleteUser)
+
+app.get(`/message`, db.GetMessages)
+app.get('/message/:id', db.GetMessageByID)
+app.post('/message', db.CreateMessage)
+
+app.post(`/allmessagebyuserid`, db.AllMessageByUserID)
+app.post(`/ReplyMessage`, db.ReplyMessage)
+app.post(`/AllConversationBySender`, db.AllConversationBySender)
 
 app.listen(port,()=>{
   console.log(`Running in port : ${port}`)
--- queries.js
+++ queries.js
@@ -19,6 +19,85 @@ const GetUsers = (req, res) => {
   })
 }
 
+const GetMessageByID = (req, res) => {
+  pool.query(`SELECT * FROM MESSAGE WHERE ID = $1`, [parseInt(req.params.id)], (error, r) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(r.rows)
+  })
+}
+
+const CreateMessage = (req, res) => {
+  const { body, parentid, senderid, receiverid, isread} = req.body
+  pool.query(`INSERT INTO MESSAGE (BODY, PARENTID, SENDERID, RECEIVERID, ISREAD) VALUES ($1, $2, $3, $4, $5)`, [body, parentid, senderid, receiverid, isread], (error, r) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).send(`message created`)
+  })
+}
+
+const AllMessageByUserID = (req, res)=>{
+  const { senderid, receiverid } = req.body
+  pool.query(`SELECT * FROM MESSAGE WHERE SENDERID = $1 AND RECEIVERID = $2 ORDER BY CREATEDDATE DESC`, [senderid, receiverid], (err, r)=>{
+    if(err){
+      throw err
+    }
+    res.status(200).json(r.rows)
+  })
+}
+
+const ReplyMessage = (req, res) => {
+  const { senderid, receiverid , body} = req.body
+  pool.query(`SELECT * FROM MESSAGE WHERE SENDERID = $1 AND RECEIVERID = $2 `, [receiverid, senderid], (err, r) => {
+    if(err){
+      throw err
+    }
+    if(r.rowCount < 1){
+      res.status(200).send(`No conversation`)
+    } else {
+      pool.query(`INSERT INTO MESSAGE (BODY, SENDERID, RECEIVERID) VALUES ($1, $2, $3)`, [body, senderid, receiverid], (er, re) => {
+        if (er) {
+          throw er
+        }
+        res.status(200).send(`message created`)
+      })
+    }    
+  })
+}
+
+const AllConversationBySender = (req, res) => {
+  const { senderid } = req.body
+  pool.query(`SELECT A.CONVERSATION, A.BODY, B.UNREAD FROM (SELECT DISTINCT ON (RECEIVERID) RECEIVERID, CONCAT(SENDERID, '-', RECEIVERID) AS CONVERSATION , CREATEDDATE, BODY 
+  FROM MESSAGE 
+  WHERE SENDERID = $1 
+  ORDER BY RECEIVERID, CREATEDDATE DESC) A
+  LEFT JOIN 
+  (SELECT RECEIVERID, SUM(CASE WHEN ISREAD=FALSE THEN 1 ELSE 0 END) AS UNREAD
+  FROM MESSAGE
+  WHERE SENDERID = $2
+  GROUP BY RECEIVERID) B
+  ON A.RECEIVERID = b.RECEIVERID`, [senderid, senderid], (err, r) => {
+    if(err) {
+      throw err
+    }
+    res.status(200).json(r.rows)
+  })
+}
+
 module.exports = {
   GetUsers,
+  GetUserByID,
+  CreateUser,
+  UpdateUser,
+  DeleteUser,
+
+  GetMessages,
+  GetMessageByID,
+  CreateMessage,
+
+  AllMessageByUserID,
+  ReplyMessage,
+  AllConversationBySender
 }
